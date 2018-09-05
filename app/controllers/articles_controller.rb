be4666c2554da36e0c144c8452c73f8ff7f2fbf9
WilsonRobert <hobento@gmail.com>
2018-09-05 19:39:36 +0000

    adding error messages and further functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,20 @@ class ArticlesController < ApplicationController
 
 def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to_article_path(@article)
-end
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    end
 
 private
 
 def article_params
   params.require(:article).permit(:title, :description)
+end
+
+def show
+  @article = Article.find(parmas[:id])
 end
