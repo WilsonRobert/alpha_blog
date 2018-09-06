9d0997283d7b2f37975a3aa22d689282075154d2
WilsonRobert <hobento@gmail.com>
2018-09-06 19:02:16 +0000

    index and other additions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+def index
+  @articles = Article.all
+end
+
 def new
   @article = Article.new
 end
@@ -23,4 +27,15 @@ end
 def show
   @article = Article.find(parmas[:id])
 end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Article was successfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
+end
+
 end
