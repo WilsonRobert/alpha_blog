a03a8022b310b6397a1c72038d9adc9d2a08eedd
WilsonRobert <hobento@gmail.com>
2018-09-06 17:11:57 +0000

    A thing

--- db/migrate/20180904202623_add_create_at_to_articles.rb
+++ db/migrate/20180904202623_add_create_at_to_articles.rb
@@ -1,5 +1,5 @@
 class AddCreateAtToArticles < ActiveRecord::Migration[5.2]
   def change
-    add_column :articls, :created_at, :datetime
+    add_column :articles, :created_at, :datetime
   end
 end
